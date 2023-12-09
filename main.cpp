67d79197f6b474c4de2047378ccd41185557a6c0
Niels Jaspers <naeliss.github@gmail.com>
2023-12-09 13:52:22 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <sstream>
+
+std::string menu(std::string &input);
+void searchName();
+void addName();
+void removeName();
+
+int main(int argc, char* argv[])
+{
+  // std::cout << "Hello World!\n";
+  
+  bool quit = false;
+
+  std::string line;
+  std::string choice;
+
+  std::ifstream file("names.txt");
+  if (!file.is_open()){
+    std::cout << "Error: File not found!\n";
+    return 1;
+  }
+
+  //while(std::getline(file, line)){
+    // std::cout << line << std::endl;
+  //}
+  
+  do{
+    int choice_int;
+
+    menu(choice);
+
+    try {
+       choice_int = std::stoi(choice);
+    }
+    catch (std::invalid_argument const &e) {
+      std::cout << "GEEN NUMMER!\n" << std::endl;
+    } 
+
+    switch(choice_int){
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        quit = true;
+        break;
+      default:
+        std::cout << "Ongeldige keuze!\n" << std::endl;
+        break;
+    }
+
+    
+    }while(!quit);
+
+  return 0;
+}
+
+std::string menu(std::string &input){
+
+  std::cout << "Wat wil je doen?" << std::endl;
+  std::cout << "\t1. Naam zoeken" << std::endl;
+  std::cout << "\t2. Naam toevoegen" << std::endl;
+  std::cout << "\t3. Naam verwijderen" << std::endl;
+  std::cout << "\t4. Stoppen" << std::endl;
+  std::cout << "Keuze: ";
+  std::cin >> input;
+
+  return input;
+}
+
+void searchName(){
+  std::string name;
+
+  std::cout << "Voer naam in die je wilt zoeken: ";
+  std::cin >> name;
+  // if(std::any_of(name.begin(), name.end(), std::isdigit)){
+  //   std::cout << "Naam mag geen cijfers bevatten!\n" << std::endl;
+  // }
+
+}
